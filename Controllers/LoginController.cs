9452db91258bb25da897e40b3b99daef0fb963b4
FrancoScaglione203 <franco.scaglione2@gmail.com>
2023-09-30 23:37:46 -0300

    Configuracion conexion con back y Login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using Data.Base;
+using Data.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TrabajoIntegradorSofttekFront.Models;
+
+namespace TrabajoIntegradorSofttekFront.Controllers
+{
+
+	public class LoginController : Controller
+    {
+        private readonly IHttpClientFactory _httpClient;
+        public LoginController(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Ingresar(LoginDto login)
+        {
+            var baseApi = new BaseApi(_httpClient);
+            var token = await baseApi.PostToApi("Login", login);
+            var resultadoLogin = token as OkObjectResult;
+            var apiResponse = JsonConvert.DeserializeObject<ApiResponse<LoginResponse>>(resultadoLogin.Value.ToString());
+            ViewBag.Nombre = apiResponse.Data.Nombre;
+            ViewBag.Cuil = apiResponse.Data.Cuil;
+
+            return View("~/Views/Home/Index.cshtml", apiResponse.Data);
+        }
+    }
+}
---
+++ Models/LoginResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace TrabajoIntegradorSofttekFront.Models
+{
+    public class LoginResponse
+    {
+        public string Nombre { get; set; }
+        public long Cuil { get; set; }
+        public string Token { get; set; }
+        public int dni { get; set; }
+        public int roleId { get; set; }
+    }
+}
