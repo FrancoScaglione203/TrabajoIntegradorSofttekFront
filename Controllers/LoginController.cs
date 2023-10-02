acb6035f4247232d741c3097382f42053366f600
FrancoScaglione203 <franco.scaglione2@gmail.com>
2023-10-02 02:00:16 -0300

    Config authorize-GetAllUsuarios-InsertUsuario

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,8 +1,12 @@
 ﻿using Data.Base;
 using Data.DTOs;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Security.Claims;
 using TrabajoIntegradorSofttekFront.Models;
+using TrabajoIntegradorSofttekFront.ViewModels;
 
 namespace TrabajoIntegradorSofttekFront.Controllers
 {
@@ -26,10 +30,32 @@ namespace TrabajoIntegradorSofttekFront.Controllers
             var token = await baseApi.PostToApi("Login", login);
             var resultadoLogin = token as OkObjectResult;
             var apiResponse = JsonConvert.DeserializeObject<ApiResponse<LoginResponse>>(resultadoLogin.Value.ToString());
-            ViewBag.Nombre = apiResponse.Data.Nombre;
-            ViewBag.Cuil = apiResponse.Data.Cuil;
 
-            return View("~/Views/Home/Index.cshtml", apiResponse.Data);
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+            Claim claimRole = new(ClaimTypes.Role, apiResponse.Data.roleId.ToString());
+
+            identity.AddClaim(claimRole);
+
+            var claimPrincipal = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimPrincipal, new AuthenticationProperties
+            {
+                ExpiresUtc = DateTime.Now.AddHours(1),
+            });
+
+            HttpContext.Session.SetString("Token", apiResponse.Data.Token);
+
+            var homeViewModel = new HomeViewModel();
+            homeViewModel.Token = apiResponse.Data.Token;
+
+            return View("~/Views/Home/Index.cshtml", homeViewModel);
+
+        }
+
+        public async Task<IActionResult> CerrarSesion()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Login");
         }
     }
 }
